8643192caabd1b6164fd5025176cf18124af7424
simon <simo274e@stud.kea.dk>
2021-04-06 15:42:03 +0200

    Lavet en bibliotek classes med en bog creater og lavet en bog classes med en constructor og getteres

--- Bog opgaven/biblioteket.java
+++ Bog opgaven/biblioteket.java
@@ -4,9 +4,11 @@ public class biblioteket {
     public Bog createBog(){
         Scanner in = new Scanner(System.in);
         System.out.println("Skriv isbNummeret , titel og udgivelsesdatoen for den bog du gerne vil søge efter");
+
         int isbNummeret = in.nextInt();
         String titel = in.next();
         int udgivelsesdato = in.nextInt();
+
         return new Bog (isbNummeret, titel, udgivelsesdato);
     }
 }
