4f6ff4b2260c4a519078cc8781000bb73c1e3665
Peter <ptrsand@gmail.com>
2021-04-06 15:19:26 +0200

    Added Line

--- Test/Hejsa.java
+++ Test/Hejsa.java
@@ -2,5 +2,6 @@ public class Hejsa {
     public static void main(String[] args) {
         System.out.println("Sup");
         System.out.println("Sup sup sup");
+        System.out.println("Sup sup sup");
     }
 }
