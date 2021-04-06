626e7fc130265425e932c666e66ec4862fc96b03
Nicolas <nkristensen1@gmail.com>
2021-04-06 15:36:08 +0200

    Kvadrat

---
+++ Test/Kvadrat.java
@@ -0,0 +1,14 @@
+public class Kvadrat {
+    public static void main(String[] args) {
+    RandomCha('*', 6);
+    }
+    public static void RandomCha(char ch, int number){
+        for (int i = 1; i <= number; i++){
+            for (int j = 1; j <= number; j++){
+                System.out.print(ch);
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
